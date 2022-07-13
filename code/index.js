e8548175646ee3c6171afd7b4ee73a5b45d6fdd8
Christian Valeri <mortlaxris@outlook.com>
2022-07-12 19:46:18 -0500

    added bootstrap and javascript

--- code/index.js
+++ code/index.js
@@ -1,14 +1,72 @@
-const btn = document.getElementsByClassName("btn")
-const texts = document.getElementsByClassName("texts")
-const form = document.getElementById("form")
+function validation (){
+    const btn = document.getElementsByClassName("btn")
+    const form = document.getElementById("form")
+    const email = document.getElementById("email").value
+    const errorMsg = document.getElementByClassName("errorMsg")
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-form.addEventListener("submit", (e) => {
-    let messages = []
-    if (texts.value === "" || texts.value == null){
-        messages.push("Email is Required")
+    if (email.match(pattern)){
+        form.classList.add("valid")
+        form.classList.remove("invalid")
+        errorMsg.innerhtml = "Your Email is Valid"
+        errorMsg.style.color = "#00ff00"
+    } else{
+        form.classList.remove("valid")
+        form.classList.remove("invalid")
+        errorMsg.innerhtml = "Please provide a proper email"
+        errorMsg.style.color = "#ff0000"
     }
+}
 
-    if (messages.length > 0){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+form.addEventListener("submit", e => {
         e.preventDefault()
-    }
+
+        checkInputs();
 })
+
+function checkInputs() {
+    const emailValue = email.value.tirm();
+
+    if (emailValue === '') {
+        setErrorFor(email, "Email cannot be empty");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "Not a valid Email");
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"] + (/</>.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+};
+*/
