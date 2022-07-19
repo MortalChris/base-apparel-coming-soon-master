1a0bd215599da5c8680eefa45c7803f77ac7f633
Christian Valeri <mortlaxris@outlook.com>
2022-07-19 17:30:56 -0500

    Testing javascript with stack overflow

--- code/index.js
+++ code/index.js
@@ -1,41 +1,70 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", evt => evt.preventDefault());
+
+const elError = form.querySelector("#error-message");
+const msgError = elError.lastElementChild;
+const imgError = elError.firstElementChild;
+form.querySelector("button").addEventListener("click", evt => {
+  // Reset to defaults
+  elError.removeAttribute("hidden"); // Allow showing
+  imgError.removeAttribute("src"); // Hide when no 'src'
+
+  if (!form.checkValidity()) {
+    msgError.textContent = "Please provide a proper email address!";
+    imgError.src = "images/icon-error.svg";
+  }
+});
+
+// Optional: Hide error until next submission-attempt (click); this improves UX (opinionated)
+form.addEventListener("change", () => elError.setAttribute("hidden", ""));
+
+
+/*  Method 2 
 function validation (){
-    const btn = document.getElementsByClassName("btn")
-    const form = document.getElementById("form")
-    const email = document.getElementById("email").value
-    const errorMsg = document.getElementByClassName("errorMsg")
-    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const form = document.getElementById("form");
+    const email = document.getElementById("email").value;
+    const errorMsg = document.getElementByClassName("errorMsg");
+    const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
     if (email.match(pattern)){
-        form.classList.add("valid")
-        form.classList.remove("invalid")
-        errorMsg.innerhtml = "Your Email is Valid"
-        errorMsg.style.color = "#00ff00"
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        errorMsg.innerhtml = "Your Email is Valid";
+        errorMsg.style.color = "#00ff00";
     } else{
-        form.classList.remove("valid")
-        form.classList.remove("invalid")
-        errorMsg.innerhtml = "Please provide a proper email"
-        errorMsg.style.color = "#ff0000"
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        errorMsg.innerhtml = "Please provide a proper email";
+        errorMsg.style.color = "#ff0000";
+    }
+
+    if (email == ""){
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        errorMsg.innerHTML = "";
+        errorMsg.style.color = "#00ff00";
     }
 }
+*/
+/*  Method 2
+const form = document.getElementById("form");
+    const email = document.getElementById("email").value;
+    const errorMsg = document.getElementById("errorMsg");
+    const error = document.getElementById("error");
+    
+  
+  form.addEventListener("submit", (e) => {
+    if(email.validity.typeMismatch){
+      e.preventDefualt();
+      error.style.opacity = 1;
+      errorMsg.style.opacity = 1;
+    } else {
+      error.style.opacity = 0;
+      errorMsg.style.opacity = 0;
+    }
+  })*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
+/* Method 3
 form.addEventListener("submit", e => {
         e.preventDefault()
 
